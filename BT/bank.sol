ff966963fe18401736f2627f4f77e7216b2e21b0
SanketJhavar <87020585+SanketJhavar@users.noreply.github.com>
2023-11-17 23:04:34 +0530

    Update bank.sol

--- BT/bank.sol
+++ BT/bank.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
 
+contract Account{
+    uint public balance;
+    address public account;
+
+    constructor()
+    {
+        account = msg.sender;
+    }
+
+    function deposit(uint amt_) public {
+        balance += amt_;
+    }
+
+    function withdraw(uint amt_) public {
+        balance -= amt_;
+    }
+
+    function get_balance() view public returns (uint){
+        return balance;
+    }
+}
